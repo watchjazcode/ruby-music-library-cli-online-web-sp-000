1e50d67e0d64c4fc20cee3d54e4b827059b44289
Jazmin M <jazminmerckel@gmail.com>
2019-11-08 02:00:19 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -68,7 +68,8 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     input = gets
-    sorted_songs = 
+    sorted_songs = Song.all.sort_by {|song| song.name } 
+    song = sorted_songs[input.to_i - 1]
     puts "Playing #{song.name} by #{song.artist.name}"
   end
   
